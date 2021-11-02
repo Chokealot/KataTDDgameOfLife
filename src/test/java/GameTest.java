24c36fdc8ce32ea11d7e07b6363e196a310cf3e7
Chokealot <casper.konyi@iths.se>
2021-11-02 11:05:17 +0100

    - Added two tests for individual cell testing: shouldNotReturnOneFromIndividualCell() && shouldReturnZeroFromIndividualCell()

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -15,5 +15,13 @@ public class GameTest {
     public void createEmptyGameGrid() {
         assertEquals(fixed25by25, game.grid);
     }
+    @Test
+    public void shouldReturnZeroFromIndividualCell() {
+        assertEquals(0, game.grid[5][5]);
+    }
+    @Test
+    public void shouldNotReturnOneFromIndividualCell() {
+        assertNotEquals(1, game.grid[7][2]);
+    }
 
 }
