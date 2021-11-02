16cb9443c1d179c5f8c4c8f760551400b900f81b
Chokealot <casper.konyi@iths.se>
2021-11-02 11:00:52 +0100

    -Added method generateGameGrid()

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,11 +5,21 @@ public class Game {
 
     public int[][] grid = new int[gridHeight][gridWidth];
 
+    public void generateGameGrid() {
+        for (int y=0; y < gridHeight; y++) {
+            for (int x=0; x < gridWidth; x++)  {
+                if (grid[y][x] == 1) System.out.print("x");
+                else System.out.print(".");
+            }
+            System.out.println();
+        }
+    }
     public void createEmptyGame() {
         for (int y=0; y < gridHeight; y++) {
             for (int x = 0; x < gridWidth; x++) {
                 grid[y][x] = 0;
             }
         }
+        System.out.println(grid);
     }
 }
